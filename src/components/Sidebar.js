77dbd857a456e6e67ee038d4ca1ba93209b4a5a1
Vlad <pruteanuvlad1611@yahoo.com>
2023-12-09 00:56:19 +0200

    add get for data

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import * as FaIcons from "react-icons/fa";
@@ -6,6 +6,7 @@ import * as AiIcons from "react-icons/ai";
 import { SidebarData } from "./SidebarData";
 import SubMenu from "./SubMenu";
 import { IconContext } from "react-icons/lib";
+import axios from 'axios';
 
 const Nav = styled.div`
 background: #DBE2EF;
@@ -46,9 +47,40 @@ width: 100%;
 
 const Sidebar = () => {
 const [sidebar, setSidebar] = useState(false);
+const [temperatura, setTemperatura] = useState(false);
+const [umiditate, setUmiditate] = useState(false);
 const showSidebar = () => setSidebar(!sidebar);
-var temperatura = 25;
-var umiditate = 25;
+
+
+useEffect(() => {
+    // Fetch data from API 1
+    const fetchData1 = async () => {
+      try {
+        const response = await axios.get('https://site-licenta-10aff3814de1.herokuapp.com/temperatura');
+        setTemperatura(response.data.temperatura);
+      } catch (error) {
+        console.error('Error fetching data from API 1:', error);
+      }
+    };
+
+    fetchData1();
+  }, []); // The empty dependencies array means this effect runs only once after the initial render
+
+  useEffect(() => {
+    // Fetch data from API 2
+    const fetchData2 = async () => {
+      try {
+        const response = await axios.get('https://site-licenta-10aff3814de1.herokuapp.com/umiditate');
+        setUmiditate(response.data.umiditate);
+      } catch (error) {
+        console.error('Error fetching data from API 2:', error);
+      }
+    };
+
+    fetchData2();
+  }, []); // The empty dependencies array means this effect runs only once after the initial render
+
+
 return (
 	<>
 	<IconContext.Provider value={{ color: "#fff" }}>
